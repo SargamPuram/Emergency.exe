89859a688d8c66eea9572046d0dfafeb1c27dd17
Sargam Puram <130867468+SargamPuram@users.noreply.github.com>
2025-04-21 00:13:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Emergency.exe',
+  description: 'A* Path finding game',
+  generator: 'SargamPuram',
 }
 
 export default function RootLayout({
